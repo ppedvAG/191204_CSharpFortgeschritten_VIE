55b975ceec37c167993824b0378a7884f50d43a3
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-04 15:23:33 +0100

    Init DelegatenUndEvents

---
+++ DelegatenUndEreignisse/DelegatenUndEreignisse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatenUndEreignisse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
