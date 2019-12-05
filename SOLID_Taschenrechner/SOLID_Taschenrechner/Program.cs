84163d0dc7bbd61adfb0f8ff0fdc9a81b0124ce7
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-05 16:24:29 +0100

    Simples "Login-System"

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
 namespace SOLID_Taschenrechner
 {
@@ -9,10 +10,74 @@ namespace SOLID_Taschenrechner
         // Bootstrapping -> Initialisieren der Software + Module
         static void Main(string[] args)
         {
-            IParser parser = new RegexParser();
-            IRechner rechner = new ModularerRechner(new Addition(), new Subtraktion(), new Division(), new Modulo());
+            IParser parser;
+            IRechner rechner;
+            string user;
 
-            new KonsolenUI(parser,rechner).Start();
+            do
+            {
+
+                Console.WriteLine("Bitte geben Sie Ihren Usernamen ein (Beenden mit 'q'):");
+                user = Console.ReadLine();
+
+                if (user == "Michi")
+                {
+                    parser = new StringSplitParser();
+                    rechner = new SimplerRechner();
+
+                    do
+                    {
+                        new KonsolenUI(parser, rechner).Start();
+                        Console.WriteLine("Neustart (n), Beenden (q), Logout (l)");
+                        string eingabe = Console.ReadLine();
+                        if (eingabe == "q")
+                            return; // Programm beenden;
+                        else if (eingabe == "l")
+                            break;
+                    } while (true); // Solange eingeloggt
+                }
+                else if (user == "Marco")
+                {
+                    parser = new RegexParser();
+                    rechner = new ModularerRechner(new Addition(), new Subtraktion());
+
+                    do
+                    {
+                        new KonsolenUI(parser, rechner).Start();
+                        Console.WriteLine("Neustart (n), Beenden (q), Logout (l)");
+                        string eingabe = Console.ReadLine();
+                        if (eingabe == "q")
+                            return; // Programm beenden;
+                        else if (eingabe == "l")
+                            break;
+                    } while (true); // Solange eingeloggt
+                }
+                else if (user == "Anna")
+                {
+                    parser = new RegexParser();
+                    rechner = new ModularerRechner(new Addition(), new Subtraktion(), new Division(), new Modulo());
+
+                    do
+                    {
+                        new KonsolenUI(parser, rechner).Start();
+                        Console.WriteLine("Neustart (n), Beenden (q), Logout (l)");
+                        string eingabe = Console.ReadLine();
+                        if (eingabe == "q")
+                            return; // Programm beenden;
+                        else if (eingabe == "l")
+                            break;
+                    } while (true); // Solange eingeloggt
+                }
+                else
+                {
+                    Console.WriteLine("Ungültiger Username");
+                }
+            } while (user != "q");
+
+            //IParser parser = new RegexParser();
+            //IRechner rechner = new ModularerRechner(new Addition(), new Subtraktion(), new Division(), new Modulo());
+
+            //new KonsolenUI(parser,rechner).Start();
         }
     }
 }
