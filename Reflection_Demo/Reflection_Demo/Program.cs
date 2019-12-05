4fe862e0696b75adb730a46c642143e56f5ee51b
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-05 16:03:57 +0100

    Reflection mit 2 DLLs

--- Reflection_Demo/Reflection_Demo/Program.cs
+++ Reflection_Demo/Reflection_Demo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,27 @@ namespace Reflection_Demo
     {
         static void Main(string[] args)
         {
+            // Typeninformation zur Laufzeit holen:
+            object o1 = 42;
+            Type objectType = o1.GetType();
+            Console.WriteLine(objectType);
 
+            // DLL zur Laufzeit laden
+            Assembly tr_dll = Assembly.LoadFrom("Taschenrechner.dll");
 
+            // Rechner verwenden:
+
+            Type tr_Type = tr_dll.GetType("Taschenrechner.Rechner");
+            Console.WriteLine(tr_Type);
+
+            // Rechner zur Laufzeit erstellen
+            object taschenrechner = Activator.CreateInstance(tr_Type);
+
+            // Methode zur Laufzeit ausführen:
+            MethodInfo method = tr_Type.GetMethod("Add", new Type[] { typeof(int), typeof(int) });
+
+            var erg = method.Invoke(taschenrechner, new object[] { 12, 3 });
+            Console.WriteLine(erg);
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
---
+++ Reflection_Demo/TrumpTaschenrechner/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    public class Rechner
+    {
+        public int Add(int z1, int z2)
+        {
+            return z1 + z2 + 50; // FAKE NEWS
+        }
+    }
+}
