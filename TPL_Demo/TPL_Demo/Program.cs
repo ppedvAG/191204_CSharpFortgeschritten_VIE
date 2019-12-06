d5d71501285293adeafa69b9d107b9a79eb80ebf
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-06 15:09:58 +0100

    PerformanceTest

--- TPL_Demo/TPL_Demo/Program.cs
+++ TPL_Demo/TPL_Demo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading; // Das braucht man für z.B. Thread.Sleep()
@@ -38,47 +40,96 @@ namespace TPL_Demo
             //cts.Cancel(); 
             #endregion
 
-            // Exception im Task
-            Task t1 = null, t2 = null, t3 = null;
-            try
+            #region Exception im Task
+            //Task t1 = null, t2 = null, t3 = null;
+            //try
+            //{
+            //    t1 = new Task(MachEinenFehler1);
+            //    t1.Start();
+
+            //    t2 = Task.Factory.StartNew(MachEinenFehler2);
+            //    t3 = Task.Run(MachEinenFehler3);
+
+            //    Console.WriteLine("Ich warte auf einen Fehler ..... ");
+
+            //    // 1) Man bekommt Exceptions auf einem anderen Kern NICHT MIT !!!!
+            //    // 2) AUSSER man wartet auf den Task bzw fragt das Resultat ab
+
+            //    Task.WaitAll(t1, t2, t3);
+            //}
+            //catch (AggregateException ex) // <-- Spezial-Exception nur für Tasks
+            //{
+            //    foreach (var einzelException in ex.InnerExceptions)
+            //    {
+            //        Console.WriteLine(einzelException.Message);
+            //    }
+            //}
+
+            //// genauer nachschauen:
+            //if (t3.IsCompleted)
+            //    Console.WriteLine("Task 3 ist fertig");
+            //if (t3.IsFaulted)
+            //{
+            //    Console.WriteLine("Task 3 hatte einen Fehler:");
+            //    Console.WriteLine(t3.Exception.Message);
+            //}
+            //if (t3.IsCanceled)
+            //{
+            //    Console.WriteLine("Task 3 wurde abgebrochen");
+            //}
+            #endregion
+
+
+            int[] durchgänge ={ 1_000, 10_000, 50_000, 100_000, 250_000, 500_000, 1_000_000,5_000_000,10_000_000,100_000_000 };
+
+            Stopwatch watch = new Stopwatch();
+
+            for (int i = 0; i < durchgänge.Length; i++)
             {
-                t1 = new Task(MachEinenFehler1);
-                t1.Start();
+                Console.WriteLine($"Durchgang Nr {durchgänge[i]}");
+                watch.Restart();
+                ForSchleife(durchgänge[i]);
+                watch.Stop();
 
-                t2 = Task.Factory.StartNew(MachEinenFehler2);
-                t3 = Task.Run(MachEinenFehler3);
+                Console.WriteLine($"For: {watch.ElapsedMilliseconds}ms");
 
-                Console.WriteLine("Ich warte auf einen Fehler ..... ");
+                watch.Restart();
+                ParallelFor(durchgänge[i]);
+                watch.Stop();
 
-                // 1) Man bekommt Exceptions auf einem anderen Kern NICHT MIT !!!!
-                // 2) AUSSER man wartet auf den Task bzw fragt das Resultat ab
-
-                Task.WaitAll(t1, t2, t3);
-            }
-            catch (AggregateException ex) // <-- Spezial-Exception nur für Tasks
-            {
-                foreach (var einzelException in ex.InnerExceptions)
-                {
-                    Console.WriteLine(einzelException.Message);
-                }
-            }
-
-            // genauer nachschauen:
-            if (t3.IsCompleted)
-                Console.WriteLine("Task 3 ist fertig");
-            if (t3.IsFaulted)
-            {
-                Console.WriteLine("Task 3 hatte einen Fehler:");
-                Console.WriteLine(t3.Exception.Message);
-            }
-            if (t3.IsCanceled)
-            {
-                Console.WriteLine("Task 3 wurde abgebrochen");
+                Console.WriteLine($"Parallel: {watch.ElapsedMilliseconds}ms");
+                Console.WriteLine("---------------------------------------------------");
             }
 
             Console.WriteLine("---ANFANG---");
             Console.ReadKey();
         }
+
+
+        static void ForSchleife(int durchgänge)
+        {
+            double[] erg = new double[durchgänge];
+            for (int i = 0; i < durchgänge; i++)
+            {
+               // File.WriteAllText($"FORIndex{i}.txt", $"Anzahl der Durchgänge: {i}");
+
+                erg[i] = ((Math.Pow(i, 0.33333333333333333333333333333333333333333333) * Math.Sin(i + 2)) / Math.Exp(i) + Math.Log(i + 1)) * Math.Sqrt(i + 100);
+            }
+        }
+
+        static void ParallelFor(int durchgänge)
+        {
+            double[] erg = new double[durchgänge];
+
+            Parallel.For(0, durchgänge, i =>
+              {
+                  //File.WriteAllText($"PARALLELIndex{i}.txt", $"Anzahl der Durchgänge: {i}");
+                  erg[i] = ((Math.Pow(i, 0.33333333333333333333333333333333333333333333) * Math.Sin(i + 2)) / Math.Exp(i) + Math.Log(i + 1)) * Math.Sqrt(i + 100);
+              });
+        }
+
+
+
         private static void MachEinenFehler1()
         {
             Thread.Sleep(3000);
@@ -102,7 +153,7 @@ namespace TPL_Demo
         {
             CancellationTokenSource source = (CancellationTokenSource)param;
 
-            while(true)
+            while (true)
             {
                 Console.Write("zZzZ...");
                 Thread.Sleep(50);
@@ -110,7 +161,7 @@ namespace TPL_Demo
                 if (source.IsCancellationRequested)
                     break; // Schleife mit break beenden
             }
-          
+
         }
 
         private static string GibDatum()
