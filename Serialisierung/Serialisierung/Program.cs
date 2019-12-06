a1cc5844eb503620fb2c87da811bc159962c257e
Michael Zöhling <michael.zoehling@outlook.com>
2019-12-06 15:28:47 +0100

    Init Serialisierung

---
+++ Serialisierung/Serialisierung/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialisierung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+    }
+}
